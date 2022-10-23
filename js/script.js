1b5dc7940ce2ba04e8858b5aaf3f2137f8f90e54
dcb95 <dcb1618@gmail.com>
2022-10-23 15:54:23 -0300

    funções para cifra de césar e base64 inseridas, atualizações no código em todo o site

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 // Variáveis do campo de seleção e das áreas de texto
-const cesar = document.querySelector('#cesar');
-const base64 = document.querySelector('#base64');
-const enterCode = document.querySelector('#enterCode');
-const closeCode = document.querySelector('#closeCode');
+let cesar = document.querySelector('#cesar');
+let base64 = document.querySelector('#base64');
+let enterCode = document.querySelector('#enterCode');
+let closeCode = document.querySelector('#closeCode');
 
 // Código do campo de seleção para escolher o método de criptografia
-const method = document.querySelector('.method');
-const select = document.querySelector('#changeCod');
-const increment = document.createElement('label');
-increment.innerHTML = `
-    <label class="increment">Incremento 1-25<input type="number"></label>
-`;
+let method = document.querySelector('.method');
+let select = document.querySelector('#changeCod');
+let increment = document.querySelector('.increment');
+let inputIncrement = document.createElement('input');
+inputIncrement.type = 'number';
+inputIncrement.max = '26';
+inputIncrement.min = '1';
+increment.append(inputIncrement);
 increment.remove();
 select.addEventListener('change', incrementFunction);
 function incrementFunction(){
-    const changeOption = select.value;
-    if(changeOption === 'Cifra de César'){
+    if(select.value === 'Cifra de César'){
         method.append(increment);
     } else{
         increment.remove();
@@ -23,18 +24,68 @@ function incrementFunction(){
 }
 
 // Código para o texto do botão mudar de acordo com o campo radial selecionado
-const buttonCripto = document.querySelector('#buttonCripto');
-const optionCode = document.querySelector('.optionCode');
+let buttonCripto = document.querySelector('#buttonCripto');
+let optionCode = document.querySelector('.optionCode');
 buttonCripto.remove();
-const typeCripto = document.querySelector('#cripto');
+let typeCripto = document.querySelector('#cripto');
 typeCripto.addEventListener('click', buttonClickCripto);
 function buttonClickCripto(){
     optionCode.append(buttonCripto);
     buttonCripto.value = 'Codificar';
 }
-const typeDescripto = document.querySelector('#descripto');
+let typeDescripto = document.querySelector('#descripto');
 typeDescripto.addEventListener('click', buttonClickDescripto);
 function buttonClickDescripto(){
     optionCode.append(buttonCripto);
     buttonCripto.value = 'Decodificar';
 }
+
+// Função para Base64
+buttonCripto.addEventListener('click', base64Codify);
+function base64Codify(){
+    let valueCode = enterCode.value;
+    let optionResult = buttonCripto.value;
+    if(select.value === 'Base64' && optionResult === 'Codificar'){
+        closeCode.value = btoa(valueCode);
+    } else if(select.value === 'Base64' && optionResult === 'Decodificar'){
+        closeCode.value = atob(valueCode);
+    }
+}
+
+// Função para Cifra de César
+buttonCripto.addEventListener('click', cesarCodify);
+function cesarCodify(){
+    let optionResultCesar = buttonCripto.value;
+    let numIncrement = (Number(inputIncrement.value) % 26); // essa variável limita o deslocamento do incremento.
+    let alphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"; // alfabeto para fazer a condificação.
+    let valueCodeCesar = enterCode.value.toLowerCase(); // essa váriavel transforma em minúscula as letras do campo de texto onde o usuário digita, para que no if os valores sejam semelhantes quando comparados com o alphabet.
+    let messageCodify = ''; // variável onde a mensagem codificada vai ser inserida.
+    if(select.value === 'Cifra de César' && optionResultCesar === 'Codificar'){
+        for(let i = 0; i < valueCodeCesar.length; i++){ // Vai ler cada caractere inserido na área de texto.
+            for(let j = 0; j < alphabet.length; j++){ // Vai ler os caracteres do alphabet, tendo de ponto de referência o 'For' que pertence.
+                if(valueCodeCesar[i] == alphabet[j]){ // condição para acontecer a criptografia.
+                    messageCodify += alphabet [j + numIncrement]; // será inserido no messageCodify a string do alphabet a partir do incremento escolhido pelo usuário.
+                    break;
+                } else if(valueCodeCesar[i] == ' '){
+                    messageCodify += ' ';
+                    break;
+                }
+            }
+        }
+        return closeCode.value = messageCodify; 
+
+    } else if(select.value === 'Cifra de César' && optionResultCesar === 'Decodificar'){
+            for(let i = 0; i < valueCodeCesar.length; i++){
+                for(let j = alphabet.length - 1; j >= 0; j--){
+                    if(valueCodeCesar[i] == alphabet[j]){
+                        messageCodify += alphabet [j - numIncrement]
+                        break;
+                    } else if(valueCodeCesar[i] == ' '){
+                        messageCodify += ' ';
+                        break;
+                    }
+                }
+            }
+            return closeCode.value = messageCodify; 
+        }
+};
